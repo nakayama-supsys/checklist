e46fdf8abad0c8f670657192447fe5e25549376a
tatara-supsys <106235868+tatara-supsys@users.noreply.github.com>
2023-03-17 16:54:49 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('h1').addEventListener('click', function() {
-const today = new Date();
-const year = today.getFullYear();
-const month = String(today.getMonth() + 1).padStart(2, '0');
-const day = String(today.getDate()).padStart(2, '0');
-const currentDate = `${year}/${month}/${day}`;
-alert(currentDate);
-});
